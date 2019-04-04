c6cfa05bda1179635c6e124a921dbc0754e9bea2
hayatzsala <hayatzs.4ever@gmail.com>
2019-04-04 03:00:22 +0300

    third change

--- runMVC.js
+++ runMVC.js
@@ -26,6 +26,16 @@ var Model=function(){
             // alert(res);
             return res;
         }
+        else if(op=='sqr'){
+             document.getElementById('yValue').value='';
+             var res= Math.pow(Number(this.x),0.5);
+            return res;
+        }
+        else if(op == 'mod'){
+             var res = Number(this.x) % Number(this.y);
+           
+            return res;
+        }
         else if(op=='clear'){
              document.getElementById('xValue').value='';
              document.getElementById('yValue').value='';
@@ -62,9 +72,9 @@ var view=function () {
 var controller =function (View) {
     this.init=function(){
 
-       // var operations=document.getElementById('operation').querySelectorAll(".opr");
-        // alert(operations.length);
-       
+      
+
+       /******************************************************/
             //multiplication *
             
             document.getElementById('mult').addEventListener('click',function(){
@@ -140,6 +150,16 @@ var controller =function (View) {
                 var op='clear';
             var xvalue=document.getElementById('xValue').value;
                  var yvalue=document.getElementById('yValue').value;
+                var mymodel=new Model();
+                View.showResult(mymodel,op);
+                            }
+            );
+
+            //square root
+            document.getElementById('sqr').addEventListener('click',function(){
+                var op='sqr';
+            var xvalue=document.getElementById('xValue').value;
+                 var yvalue=document.getElementById('yValue').value;
             if(xvalue != '' && yvalue!='' && xvalue!=null && yvalue !=null){
                  
                 var mymodel=new Model();
@@ -150,6 +170,25 @@ var controller =function (View) {
                 alert("enter value");
                 return;
                             }});
+
+            //module
+            document.getElementById('mod').addEventListener('click',function(){
+                var op='mod';
+            var xvalue=document.getElementById('xValue').value;
+                 var yvalue=document.getElementById('yValue').value;
+            if(xvalue != '' && yvalue!='' && xvalue!=null && yvalue !=null){
+                 
+                var mymodel=new Model();
+                mymodel.setValues(xvalue,yvalue);
+                View.showResult(mymodel,op);
+                            }
+            else{
+                alert("enter value");
+                return;
+                            }});
+
+/******************************************************************************************/
+           
        
         }
         //   document.getElementById('add').addEventListener('click', function(){
@@ -167,10 +206,32 @@ var controller =function (View) {
         //                     }
             
         // });
+        
 
+              this.write=function(){
+                var flag;
+                document.getElementById('xValue').addEventListener('click',function(){flag=1; });
+
+                if(flag==1){
+                    var number=document.getElementById('numbers').querySelectorAll('num');
+                for (var i = 0; i < number.length; i++) {
+                    document.getElementById('xValue').value +=number[i].id.value;
+                }
+
+                }
+                 document.getElementById('yValue').addEventListener('click',function(){flag=2; });
+                 if(flag==2){
+                    var number=document.getElementById('numbers').querySelectorAll('num');
+                for (var i = 0; i < number.length; i++) {
+                    document.getElementById('yValue').value +=number[i].id.value;
+                }
+
+                }
+              }
+                  
     }
   
 
         var myview = new view();
         var mycontroller = new controller(myview);
-        mycontroller.init();
+        mycontroller.init();              
